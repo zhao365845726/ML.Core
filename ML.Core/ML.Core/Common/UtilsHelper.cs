126a99decfd9c0cc22e576780a57c1afbfc9c7df
zhao365845726@163.com <zhao365845726@163.com>
2021-05-21 23:51:09 +0800

    增加部分工具类

---
+++ ML.Core/ML.Core/Common/UtilsHelper.cs
@@ -0,0 +1,63 @@
+﻿using Senparc.Ncf.Core.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ML.Core
+{
+    public class UtilsHelper
+    {
+        public static string RegexFormatContent(string content)
+        {
+            string result = Regex.Replace(content, "<figure class=\"table\">", "");
+            result = Regex.Replace(result, "</figure>", "");
+            return result;
+        }
+
+        ///编码
+        public static string EncodeBase64(string code_type, string code)
+        {
+            string encode = "";
+            byte[] bytes = Encoding.GetEncoding(code_type).GetBytes(code);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = code;
+            }
+            return encode;
+        }
+        ///解码
+        public static string DecodeBase64(string code_type, string code)
+        {
+            //var val1 = EncodeBase64("utf-8", "<tr><td>你好啊</td></tr>");
+
+            //var val2 = DecodeBase64("utf-8", "PHRyPjx0ZD7kvaDlpb3llYo8L3RkPjwvdHI+");
+
+            string decode = "";
+            byte[] bytes = Convert.FromBase64String(code);
+            try
+            {
+                decode = Encoding.GetEncoding(code_type).GetString(bytes);
+            }
+            catch
+            {
+                decode = code;
+            }
+            return decode;
+        }
+
+        public static string EncryptionPassword(string password, string salt, bool isMD5Password)
+        {
+            string md5 = password.ToUpper().Replace("-", "");
+            if (!isMD5Password)
+            {
+                md5 = MD5.GetMD5Code(password, "").Replace("-", ""); //原始MD5
+            }
+            return MD5.GetMD5Code(md5, salt).Replace("-", ""); //再加密
+        }
+    }
+}
---
+++ ML.Core/ML.Core/Security/SenparcRedis.cs
@@ -0,0 +1,55 @@
+﻿using Senparc.CO2NET.Cache;
+using Senparc.CO2NET.Cache.CsRedis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Core
+{
+    public class SenparcRedis
+    {
+        public SenparcRedis()
+        {
+        }
+
+        public static void Set(string key, string value, TimeSpan span)
+        {
+            CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+            var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            cache.Set($"ML:{key}", value, span, true);
+        }
+
+        public static void Set(string key, object value, int tls)
+        {
+            CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+            var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            TimeSpan span = DateTime.Now.AddSeconds(tls) - DateTime.Now;
+            cache.Set($"ML:{key}", value, span, true);
+        }
+
+        public static void Set(string key, string value, int tls)
+        {
+            CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+            var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            TimeSpan span = DateTime.Now.AddSeconds(tls) - DateTime.Now;
+            cache.Set($"ML:{key}", value, span, true);
+        }
+
+        public static object Get(string key)
+        {
+            //如需立即改用某种缓存策略，只需要这样做（注意：是全局立即生效）：
+            CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+            var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            object obj = cache.Get<object>($"ML:{key}", true);
+            return obj;
+        }
+
+        public static void Remove(string key)
+        {
+            //如需立即改用某种缓存策略，只需要这样做（注意：是全局立即生效）：
+            CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+            var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            cache.RemoveFromCache($"ML:{key}", true);
+        }
+    }
+}
