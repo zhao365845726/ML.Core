961d6861cfcf7bb267b9aef895fe9a3b494be5da
zhao365845726@163.com <zhao365845726@163.com>
2021-09-08 08:55:10 +0800

    m

--- ML.Core/ConsoleApp1/Program.cs
+++ ML.Core/ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using ML.Core;
-using System.Collections.Generic;
-using ML.Core.Assemblies;
 
 namespace ConsoleApp1
 {
@@ -9,48 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            GetAssemblyDictionaryResult();
-            //TestCopyFolder();
-            Console.ReadLine();
-        }
-
-        public static void GetAssemblyDictionaryResult()
-        {
-            //string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"/nuget/";
-            ////string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-
-            string path = @"F:\local_dll";
-            Console.WriteLine(path);
-            AssemblyHandler assemblyHandler = new AssemblyHandler(path);
-            var aa = assemblyHandler.GetAssemblyDictionaryResult("P-AutoMate_TaskScheduler", "P_AutoMate_TaskScheduler.DatabaseModel");
-        }
-
-        static void TestCopyFolder()
-        {
-            string rootPath = @"E:\_GitHub.Contribution\NCF\src\Senparc.Web\wwwroot\NcfDocs";
-            string sourcePath = $"{rootPath}\\branch\\v1.0";
-            string destPath = $"{rootPath}\\v1.0";
-            Dictionary<string, string> dicExclude = new Dictionary<string, string>();
-            dicExclude.Add("FOLDER", ".git");
-            DirFileHelper.CopyFolder(sourcePath, destPath, dicExclude);
-        }
-
-        //static void TestGetLogicalDrives()
-        //{
-        //    string strInfo = string.Empty;
-        //    string[] saDrives = Directory.GetLogicalDrives();
-        //    foreach (string item in saDrives)
-        //    {
-        //        WriteLog(item);
-        //    }
-        //}
-
-        static void WriteLog(string strInfo)
-        {
             Console.WriteLine("Hello World!");
-            Console.WriteLine($"{strInfo}");
-            Console.WriteLine($"");
-            
         }
     }
 }
---
+++ ML.Core/ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        public static void GetAssemblyDictionaryResult()
+        {
+            //string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"/nuget/";
+            ////string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            //string path = @"F:\local_dll";
+            //string path = @"F:\local_dll\P_NetConfChina\netstandard2.1";
+            //Console.WriteLine(path);
+            //AssemblyHandler assemblyHandler = new AssemblyHandler(path);
+            //var aa = assemblyHandler.GetAssemblyDictionaryResult("P-NetConfChina", "P_NetConfChina.DatabaseModel");
+        }
+
+        //static void TestCopyFolder()
+        //{
+        //    string rootPath = @"E:\_GitHub.Contribution\NCF\src\Senparc.Web\wwwroot\NcfDocs";
+        //    string sourcePath = $"{rootPath}\\branch\\v1.0";
+        //    string destPath = $"{rootPath}\\v1.0";
+        //    Dictionary<string, string> dicExclude = new Dictionary<string, string>();
+        //    dicExclude.Add("FOLDER", ".git");
+        //    DirFileHelper.CopyFolder(sourcePath, destPath, dicExclude);
+        //}
+
+        //static void TestGetLogicalDrives()
+        //{
+        //    string strInfo = string.Empty;
+        //    string[] saDrives = Directory.GetLogicalDrives();
+        //    foreach (string item in saDrives)
+        //    {
+        //        WriteLog(item);
+        //    }
+        //}
+
+        static void WriteLog(string strInfo)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{strInfo}");
+            Console.WriteLine($"");
+
+        }
+    }
+}
