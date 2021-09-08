ce4ae8cf6a0143148285961986f45588cfd2b066
zhao365845726@163.com <zhao365845726@163.com>
2021-09-08 09:01:39 +0800

    优化已知问题

--- ML.Core/ConsoleApp1/Program.cs
+++ ML.Core/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ML.Core.Assemblies;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            GetAssemblyDictionaryResult();
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        public static void GetAssemblyDictionaryResult()
+        {
+            //string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"/nuget/";
+            ////string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            //string path = @"F:\local_dll";
+            string path = @"F:\local_dll\P_NetConfChina\netstandard2.1";
+            Console.WriteLine(path);
+            AssemblyHandler assemblyHandler = new AssemblyHandler(path);
+            var aa = assemblyHandler.GetAssemblyDictionaryResult("P-NetConfChina", "P_NetConfChina.DatabaseModel");
         }
     }
 }
