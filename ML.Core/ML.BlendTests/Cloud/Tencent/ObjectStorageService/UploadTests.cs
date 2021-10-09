714cc3170f3df8f25de3b2d0df6df84cb7bc75b1
zhao365845726@163.com <zhao365845726@163.com>
2021-10-09 17:03:28 +0800

    优化

--- ML.Core/ML.BlendTests/Cloud/Tencent/ObjectStorageService/UploadTests.cs
+++ ML.Core/ML.BlendTests/Cloud/Tencent/ObjectStorageService/UploadTests.cs
@@ -15,11 +15,11 @@ namespace ML.Blend.Cloud.Tencent.ObjectStorageService.Tests
         public void SimpleFileTest()
         {
             string filePath = @"E:\_GitHub\ML.Core\ML.Core\ConsoleApp1\config.json";
-            var entity = GetEntities(filePath, "tencent");
+            var entity = GetEntities(filePath, "tencent-test");
             Upload upload = new Upload(entity.accessKeyId,entity.accessKeySecret,entity.region);
             var cosxml = upload.CreateClient();
-            string strObjectName = DateTime.Now.ToString("yyyyMMddHHmmss");
-            var result = upload.SimpleFile(cosxml, entity.bucketName, strObjectName, @"F:\source\image\002.jpg");
+            string strObjectName = $"industry/coal/shanxi_kexing_group/nanyang/{DateTime.Now.ToString("yyyyMMddHHmmss")}";
+            var result = upload.SimpleFile(cosxml, entity.bucketName, strObjectName, @"F:\source\image\003.jpg");
             Console.WriteLine($"https://{entity.defaultEndPoint}/{strObjectName}");
             Console.WriteLine($"Hello SimpleFileTest {entity.accessKeyId}");
         }
