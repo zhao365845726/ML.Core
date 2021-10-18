fb797e9175bfc2f57816155312d983efecb00b00
zhao365845726@163.com <zhao365845726@163.com>
2021-10-18 18:23:07 +0800

    m

--- ML.Core/ML.Core/Http/HttpHelper.cs
+++ ML.Core/ML.Core/Http/HttpHelper.cs
@@ -327,6 +327,53 @@ namespace ML.Core
             return result;
         }
 
+
+        /// <summary>
+        /// 模拟提交数据
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static string HttpPostData(string url, string param)
+        {
+            var result = string.Empty;
+            //注意提交的编码 这边是需要改变的 这边默认的是Default：系统当前编码
+            byte[] postData = Encoding.UTF8.GetBytes(param);
+
+
+            // 设置提交的相关参数 
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            Encoding myEncoding = Encoding.UTF8;
+            request.Method = "POST";
+            request.KeepAlive = false;
+            request.AllowAutoRedirect = true;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727; .NET CLR  3.0.04506.648; .NET CLR 3.5.21022; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)";
+            request.ContentLength = postData.Length;
+            //request.Headers.Add("Content-type","application/json; charset=utf-8");
+
+            // 提交请求数据 
+            System.IO.Stream outputStream = request.GetRequestStream();
+            outputStream.Write(postData, 0, postData.Length);
+            outputStream.Close();
+
+            HttpWebResponse response;
+            Stream responseStream;
+            StreamReader reader;
+            string srcString;
+            response = request.GetResponse() as HttpWebResponse;
+            responseStream = new System.IO.Compression.GZipStream(response.GetResponseStream(), System.IO.Compression.CompressionMode.Decompress);
+
+            //做了压缩的
+            //responseStream = response.GetResponseStream();
+            reader = new System.IO.StreamReader(responseStream, Encoding.UTF8);
+            srcString = reader.ReadToEnd();
+            result = srcString;   //返回值赋值
+            reader.Close();
+
+            return result;
+        }
+
         /// <summary>
         /// 模拟授权提交数据
         /// </summary>
---
+++ ML.Core/ML.CoreTests/Http/HttpHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ML.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Core.Tests
+{
+    [TestClass()]
+    public class HttpHelperTests
+    {
+        [TestMethod()]
+        public void GetClientIPAddressTest()
+        {
+            var result = HttpHelper.GetClientIPAddress();
+            Console.Write($"Hello GetClientIPAddressTest Result is : {result}");
+        }
+
+        [TestMethod()]
+        public void HttpPostDataTest()
+        {
+            RandomHelper rh = new RandomHelper();
+            string id = "ozzdgc2gkxf6o";
+            string captcha = string.Empty;
+            string network = "wifi";
+            string dx_token = rh.GenerateRandomNumber(40);
+            string dx_captcha_token = string.Empty;
+            string hx_info_vid = "77ummtv3d3gmg";
+            //string hx_info_uid = "oFKxuw_IT6p169KFNmTO5Bxbe8f4";
+            string hx_info_uid = rh.GenerateRandomNumber(28);
+            string hx_info_wnw = "0";
+            string hx_info_time = DateTimeHelper.ConvertToUnix(DateTime.Now).ToString();
+            string hx_info_token = rh.GenerateRandomNumber(32);
+            string param = $"data[id][]={id}&data[captcha]={captcha}&data[network]={network}&data[dx_token]={dx_token}&data[dx_captcha_token]={dx_captcha_token}&data[hx_info][vid]={hx_info_vid}&data[hx_info][uid]={hx_info_uid}&data[hx_info][wnw]={hx_info_wnw}&data[hx_info][time]={hx_info_time}&data[hx_info][token]={hx_info_token}";
+            var result = HttpHelper.HttpPostData("https://77ummtv3d3gmg.v.jisutp.com/sendVote", param);
+            Console.WriteLine(result);
+        }
+    }
+}
