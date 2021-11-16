fcf564a13d1abaf19bc0960044e4885527f0bc15
zhao365845726@163.com <zhao365845726@163.com>
2021-11-16 21:01:28 +0800

    m

---
+++ ML.Core/ML.CoreTests/Assemblies/AssemblyHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ML.Core.Assemblies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Core.Assemblies.Tests
+{
+    [TestClass()]
+    public class AssemblyHandlerTests
+    {
+        [TestMethod()]
+        public void GetAssemblyDictionaryResultTest()
+        {
+            string path = @"F:\local_dll\P-ElectromechanicalWell\netstandard2.1";
+            string assemblyName = "P-ElectromechanicalWell";
+            string filterWords = "P_ElectromechanicalWell.DatabaseModel";
+
+            AssemblyHandler assemblyHandler = new AssemblyHandler(path);
+            assemblyHandler.GetAssemblyDictionaryResult(assemblyName, filterWords);
+        }
+    }
+}
