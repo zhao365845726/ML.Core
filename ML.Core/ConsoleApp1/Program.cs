da14096b0bf4ad51c5aeb88676891cbe74b3c45b
zhao365845726@163.com <zhao365845726@163.com>
2022-02-04 23:58:20 +0800

    m

--- ML.Core/ConsoleApp1/Program.cs
+++ ML.Core/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
@@ -25,10 +26,73 @@ namespace ConsoleApp1
         {
             //TestStudent();
             //TestDelegate();
-            TestFunc();
+            //TestFunc();
             //TestTuple();
             //TestValueTuple();
             //TestSystemInfo();
+            TestLambda();
+        }
+
+        /// <summary>
+        /// https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/concepts/expression-trees/
+        /// https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/concepts/expression-trees/how-to-use-expression-trees-to-build-dynamic-queries
+        /// https://docs.microsoft.com/zh-cn/dotnet/framework/reflection-and-codedom/dynamic-language-runtime-overview
+        /// https://docs.microsoft.com/zh-cn/dotnet/csharp/language-reference/operators/lambda-expressions
+        /// </summary>
+        static void TestLambda()
+        {
+            //创建Lambda表达式
+            Expression<Func<int, bool>> lambda = num => num < 5;
+
+            // Add the following using directive to your code file:  
+            // using System.Linq.Expressions;  
+
+            // Manually build the expression tree for
+            // the lambda expression num => num < 5.  
+            ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+            ConstantExpression five = Expression.Constant(6, typeof(int));
+            BinaryExpression numLessThanFive = Expression.LessThan(numParam, five);
+            Expression<Func<int, bool>> lambda1 =
+                Expression.Lambda<Func<int, bool>>(
+                    numLessThanFive,
+                    new ParameterExpression[] { numParam });// Creating a parameter expression.  
+            ParameterExpression value = Expression.Parameter(typeof(int), "value");
+
+            // Creating an expression to hold a local variable.
+            ParameterExpression result = Expression.Parameter(typeof(int), "result");
+
+            // Creating a label to jump to from a loop.  
+            LabelTarget label = Expression.Label(typeof(int));
+
+            // Creating a method body.  
+            BlockExpression block = Expression.Block(
+                // Adding a local variable.  
+                new[] { result },
+                // Assigning a constant to a local variable: result = 1  
+                Expression.Assign(result, Expression.Constant(1)),
+                    // Adding a loop.  
+                    Expression.Loop(
+                       // Adding a conditional block into the loop.  
+                       Expression.IfThenElse(
+                           // Condition: value > 1  
+                           Expression.GreaterThan(value, Expression.Constant(1)),
+                           // If true: result *= value --  
+                           Expression.MultiplyAssign(result,
+                               Expression.PostDecrementAssign(value)),
+                           // If false, exit the loop and go to the label.  
+                           Expression.Break(label, result)
+                       ),
+                   // Label to jump to.  
+                   label
+                )
+            );
+
+            // Compile and execute an expression tree.  
+            int factorial = Expression.Lambda<Func<int, int>>(block, value).Compile()(5);
+
+            Console.WriteLine(factorial);
+            // Prints 120.
+            Console.WriteLine(lambda1);
         }
 
         static void TestSystemInfo()
