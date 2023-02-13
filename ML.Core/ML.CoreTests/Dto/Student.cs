c27de3988f77a52cf293ab9e3144efe6fae13a67
zhao365845726@163.com <zhao365845726@163.com>
2023-02-13 18:43:38 +0800

    增加Json转xml

---
+++ ML.Core/ML.CoreTests/Dto/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.CoreTests.Dto
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ML.Core/ML.CoreTests/Json/JsonHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ML.Core;
+using ML.CoreTests.Dto;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+
+namespace ML.Core.Tests
+{
+    [TestClass()]
+    public class JsonHelperTests
+    {
+        [TestMethod()]
+        public void JsonToEntityTest()
+        {
+            List<Student> list = new List<Student>();
+            for (int i=1;i< 3;i++)
+            {
+                Student stu = new Student()
+                {
+                    Id = i,
+                    Name = "Test",
+                    Description = "Test",
+                    Age = 18 + i
+                };
+                list.Add(stu); 
+            }
+
+            string result = JsonHelper.ToJson(list[0]);
+
+            Console.WriteLine(result);
+
+            var aa = JsonHelper.JsonToXml(result);
+
+
+            //string Xml = result;
+            //XmlDictionaryReader reader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(Xml), XmlDictionaryReaderQuotas.Max);
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(reader);
+            //Xml = doc.InnerXml;
+
+            Console.WriteLine(aa.InnerXml);
+        }
+    }
+}
