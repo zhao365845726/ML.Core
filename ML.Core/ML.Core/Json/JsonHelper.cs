d3223cf23546a2fadb70e65022c50c570a0d8ccf
zhao365845726@163.com <zhao365845726@163.com>
2023-06-05 11:54:36 +0800

    优化其他方法

--- ML.Core/ML.Core/Json/JsonHelper.cs
+++ ML.Core/ML.Core/Json/JsonHelper.cs
@@ -167,5 +167,35 @@ namespace ML.Core
             var result = JsonConvert.DeserializeObject<T>(configInfo[section].ToString());
             return result;
         }
+
+        /// <summary>
+        /// 广联达专用
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ConvertJsonString(string str)
+        {
+            //格式化json字符串
+            JsonSerializer serializer = new JsonSerializer();
+            TextReader tr = new StringReader(str);
+            JsonTextReader jtr = new JsonTextReader(tr);
+            object obj = serializer.Deserialize(jtr);
+            if (obj != null)
+            {
+                StringWriter textWriter = new StringWriter();
+                JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+                {
+                    Formatting = Formatting.Indented,
+                    Indentation = 4,
+                    IndentChar = ' '
+                };
+                serializer.Serialize(jsonWriter, obj);
+                return textWriter.ToString();
+            }
+            else
+            {
+                return str;
+            }
+        }
     }
 }
